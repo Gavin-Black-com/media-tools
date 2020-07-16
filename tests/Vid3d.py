76c563bec90388689a3953658bdf1e8294f0e5a8
Gavin <>
2020-07-16 00:07:30 -0400

    Crop the image

--- tests/Vid3d.py
+++ tests/Vid3d.py
@@ -5,16 +5,17 @@ import VidLib
 import Image3d
 import PIL
 
-VidLib.create(size=400)
-for x in range(30):
+VidLib.create(size=300)
+for x in range(360):
     Image3d.start_3d()
     d = 0.03*x;
     Image3d.make_voxels([d,d,d])
     Image3d.draw_line((d,d,d),(10,10,10))
     Image3d.ax.view_init(30, x)
     p = Image3d.to_pil()
-    p = p.resize((400,400))
-    VidLib.img.paste(p, (0,0,400,400))
+    p = p.crop((100, 100, 400, 400))
+  #  p = p.resize((300,300))
+    VidLib.img.paste(p, (0,0,300,300))
     VidLib.text("3D Test")
     VidLib.next()
     print(x)
