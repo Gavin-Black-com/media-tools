e561f2843c5d1ce144d5d17597dc4c9b479fdd38
Gavin <>
2020-07-16 00:01:09 -0400

    Got rid of pixel loops

--- tests/Vid3d.py
+++ tests/Vid3d.py
@@ -6,7 +6,7 @@ import Image3d
 import PIL
 
 VidLib.create(size=400)
-for x in range(360):
+for x in range(30):
     Image3d.start_3d()
     d = 0.03*x;
     Image3d.make_voxels([d,d,d])
@@ -14,10 +14,7 @@ for x in range(360):
     Image3d.ax.view_init(30, x)
     p = Image3d.to_pil()
     p = p.resize((400,400))
-    px = p.load()
-    for i in range(400):
-        for j in range(400):
-            VidLib.pixels[i,j] = px[i,j]
+    VidLib.img.paste(p, (0,0,400,400))
     VidLib.text("3D Test")
     VidLib.next()
     print(x)
